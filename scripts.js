5afd51ff36454ec7a39e9c70d690f3b70410a5c6
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2021-09-14 14:06:21 -0300

    main menu finished

---
+++ scripts.js
@@ -0,0 +1,42 @@
+var mainMenu = (function () {
+  var mainMenu = {};
+
+  mainMenu.show = function () {
+    document.querySelector(".main-nav").classList.add("opened");
+  };
+
+  mainMenu.hide = function () {
+    document.querySelector(".main-nav").classList.remove("opened");
+  };
+
+  mainMenu.selectPage = function (element) {
+    var targetPage = document.querySelector(element);
+
+    if (!targetPage) {
+      return;
+    }
+
+    var targetPageOrder = 0;
+    var pages = document.querySelectorAll(".main-nav .nav-page");
+
+    for (let i = 0; i < pages.length; i++) {
+      if (pages[i].id === targetPage.id) {
+        targetPageOrder = i;
+      }
+
+      pages[i].classList.remove("selected");
+    }
+
+    for (let i = 0; i < pages.length; i++) {
+      pages[i].style.left = "-" + targetPageOrder * 100 + "%";
+    }
+
+    targetPage.classList.add("selected");
+  };
+
+  return {
+    show: mainMenu.show,
+    hide: mainMenu.hide,
+    selectPage: mainMenu.selectPage,
+  };
+})();
